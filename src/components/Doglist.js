7f5748f7813fdfa629d2b5605a00fedbe839c887
JewelBreeveld <rjbreeveld@gmail.com>
2019-02-27 11:13:40 +0100

    doglist v1

---
+++ src/components/Doglist.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import * as request from 'superagent'
+
+export default class DogsList extends Component {
+    state = { dogBreeds: null}
+    
+    componentDidMount() {
+        request
+        .get('https://dog.ceo/api/breeds/list/all')
+        .then(console.log)
+        .catch(console.error)
+    }
+
+    render () {
+        return (
+            <div className="dogs-list">
+            <h1>Dogs List</h1>
+            { this.state.dogBreeds === null & "Loading..."}
+            </div>
+        )
+    }
+}
