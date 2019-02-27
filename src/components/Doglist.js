1da6096294e1d3922c18c3d036ab976d56d82b4e
JewelBreeveld <rjbreeveld@gmail.com>
2019-02-27 16:17:47 +0100

    finished version

--- src/components/Doglist.js
+++ src/components/Doglist.js
@@ -1,22 +1,37 @@
 import React, {Component} from 'react'
-import * as request from 'superagent'
+import { Link } from 'react-router-dom'
 
-export default class DogsList extends Component {
-    state = { dogBreeds: null}
-    
-    componentDidMount() {
-        request
-        .get('https://dog.ceo/api/breeds/list/all')
-        .then(console.log)
-        .catch(console.error)
+export default class DogList extends Component {
+
+    renderDogBreed(breed) {
+        return  <li key={breed}>
+                    <Link to={ `/dog-breeds/${breed}` }>{breed}</Link>
+                </li>
     }
-
+    
     render () {
+       
+        const { dogBreeds } = this.props
+
         return (
             <div className="dogs-list">
             <h1>Dogs List</h1>
-            { this.state.dogBreeds === null & "Loading..."}
+            { !dogBreeds 
+                && 'Loading...' }
+            { dogBreeds 
+                    && <ul>{dogBreeds.map(this.renderDogBreed) } </ul>
+            }        
             </div>
         )
     }
 }
+
+
+
+// line 30 - 34 (below, was underneath <h1>Dogs List</h1>) 'if' this.state.dogBreeds === null 'then' display "Loading ..." AND 'if' this.state.dogBreeds !== null 'then' display array of dogbreeds. 
+// { this.state.dogBreeds === null 
+//     && 'Loading...' }
+// { <ul>
+//     { this.state.dogBreeds !== null 
+//         && this.state.dogBreeds.map(breed => <li key={breed}>{breed}</li>) }
+// </ul> }
