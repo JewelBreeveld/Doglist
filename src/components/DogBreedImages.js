031c26dbd57266111653bb7f6538a7612550b842
JewelBreeveld <rjbreeveld@gmail.com>
2019-06-13 09:56:25 +0200

    Added unique key props to ul

--- src/components/DogBreedImages.js
+++ src/components/DogBreedImages.js
@@ -1,19 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function DogBreedImages (props) {
-    
-    return (
-        
-        <div className="dog-breed-images">
-        <h1>Dogs Breed Images</h1>
-        This page will show images of the { props.match.params.breed } breed.
-
-        <Link to="/">Go back to the index</Link>
-        <div>
-            { props.images && props.images.map(url => <img src={ url } alt="Dog" />) }
-            { props.images && 'Loading...' }
-        </div>
-        </div>
-    )
+export default function DogBreedImages(props) {
+  return (
+    <div className="dog-breed-images">
+      <h1>Dogs Breed Images</h1>
+      This page will show images of the {props.match.params.breed} breed.
+      <Link to="/">Go back to the index</Link>
+      <div>
+        {props.images &&
+          props.images.map(url => <img src={url} alt="Dog" key={url} />)}
+        {props.images && "Loading..."}
+      </div>
+    </div>
+  );
 }
--- src/components/Doglist.js
+++ src/components/Doglist.js
@@ -1,37 +1,36 @@
-import React, {Component} from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class DogList extends Component {
+  renderDogBreed(breed) {
+    return (
+      <li key={breed}>
+        <Link to={`/dog-breeds/${breed}`}>{breed}</Link>
+      </li>
+    );
+  }
 
-    renderDogBreed(breed) {
-        return  <li key={breed}>
-                    <Link to={ `/dog-breeds/${breed}` }>{breed}</Link>
-                </li>
-    }
-    
-    render () {
-       
-        const { dogBreeds } = this.props
+  render() {
+    const { dogBreeds } = this.props;
 
-        return (
-            <div className="dogs-list">
-            <h1>Dogs List</h1>
-            { !dogBreeds 
-                && 'Loading...' }
-            { dogBreeds 
-                    && <ul>{dogBreeds.map(this.renderDogBreed) } </ul>
-            }        
-            </div>
-        )
-    }
+    return (
+      <div className="dogs-list">
+        <h1>Dogs List</h1>
+        {!dogBreeds && "Loading..."}
+        {dogBreeds && (
+          <ul key={this.renderDogBreed}>
+            {dogBreeds.map(this.renderDogBreed)}{" "}
+          </ul>
+        )}
+      </div>
+    );
+  }
 }
 
-
-
-// line 30 - 34 (below, was underneath <h1>Dogs List</h1>) 'if' this.state.dogBreeds === null 'then' display "Loading ..." AND 'if' this.state.dogBreeds !== null 'then' display array of dogbreeds. 
-// { this.state.dogBreeds === null 
+// line 30 - 34 (below, was underneath <h1>Dogs List</h1>) 'if' this.state.dogBreeds === null 'then' display "Loading ..." AND 'if' this.state.dogBreeds !== null 'then' display array of dogbreeds.
+// { this.state.dogBreeds === null
 //     && 'Loading...' }
 // { <ul>
-//     { this.state.dogBreeds !== null 
+//     { this.state.dogBreeds !== null
 //         && this.state.dogBreeds.map(breed => <li key={breed}>{breed}</li>) }
 // </ul> }
